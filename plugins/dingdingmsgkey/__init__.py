c27de4cb5529f97b0e7af942ed8046df8457123e
skysbird <skysbird@gmail.com>
2025-07-15 15:41:01 +0800

    pluginush

--- plugins/dingdingmsgkey/__init__.py
+++ plugins/dingdingmsgkey/__init__.py
@@ -21,7 +21,7 @@ class DingdingMsgKey(_PluginBase):
     # 插件图标
     plugin_icon = "Dingding_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.12"
     # 插件作者
     plugin_author = "skysbird"
     # 作者主页
